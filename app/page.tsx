39a65a0c2ea02ec14c25d3684e822f1215dcd996
tomix421q <zilka.tomas421@gmail.com>
2024-08-19 13:40:50 +0200

    final v6

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,25 @@
 import LoadingCards from '@/components/card/LoadingCards'
 import CategoriesList from '@/components/home/CategoriesList'
-// import PropertiesContainer from '@/components/home/PropertiesContainer'
-import dynamic from 'next/dynamic'
+import PropertiesContainer from '@/components/home/PropertiesContainer'
+// import dynamic from 'next/dynamic'
 import { Suspense } from 'react'
 
 // nacitanie loading pri server side componente
-const PropertiesContainer = dynamic(() => import('@/components/home/PropertiesContainer'), {
-  loading: () => <LoadingCards />,
-  ssr: false,
-})
+// const PropertiesContainer = dynamic(() => import('@/components/home/PropertiesContainer'), {
+//   loading: () => <LoadingCards />,
+//   ssr: false,
+// })
 
 function HomePage({ searchParams }: { searchParams: { category?: string; search?: string } }) {
   // console.log(searchParams)
   return (
     <section>
-      <CategoriesList category={searchParams?.category} search={searchParams?.search} />
+      <CategoriesList category={searchParams.category} search={searchParams.search} />
 
       {/* nacitanie loadingu pri client side [Suspense] */}
-      {/* <Suspense fallback={<LoadingCards />}> */}
-      <PropertiesContainer category={searchParams?.category} search={searchParams?.search} />
-      {/* </Suspense> */}
+      <Suspense fallback={<LoadingCards />}>
+        <PropertiesContainer category={searchParams.category} search={searchParams.search} />
+      </Suspense>
     </section>
   )
 }
